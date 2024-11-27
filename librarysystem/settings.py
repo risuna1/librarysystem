14d13fe7142787b4723360808e01399aa80c8a10
Risuna <51692915+risuna1@users.noreply.github.com>
2024-11-27 14:49:38 +0900

    Update settings.py

--- librarysystem/settings.py
+++ librarysystem/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 MEDIA_URL = '/media/'
